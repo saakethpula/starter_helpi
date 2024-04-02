6a3968d75d85dc0e0e8e4534f11965e2647b7d17
brendonu@udel.edu <brendonuzoigwe@wifi-roaming-128-4-113-106.wifi.udel.edu>
2024-04-02 16:38:12 -0400

    added names

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save.
+        Brendon Uzoigwe, Girish Sista, Saaketh Pula
         </p>
         <a
           className="App-link"
