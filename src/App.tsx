f7076671395565e8aaf9603a5e8be003e20cda77
Saaketh <saakethp@udel.edu>
2024-04-08 15:31:44 -0400

    changes to visibility

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
     setKey(event.target.value);
   }
   return (
+    <div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
